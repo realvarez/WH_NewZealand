c2a2a1074659064c63a1f940eacf74698a19def4
realvarez <ricardo.alvarezz@usach.cl>
2024-10-16 01:30:39 -0300

    ADD: Readme

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Scrapping New Zealand WH Formularies
+This is a repo developed to automate the formularies of the postulation to the WH in New Zealand
+Require:
+    - Firefox
+    - Python (Tested in 3.11)
--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ python -m venv .venv
 source .venv/Scripts/activate
 pip install -r requirements.txt
 cp .env_example .env
-echo "Now, complete the .env file with your data" 
+echo "Now, complete the .env file with your data"
